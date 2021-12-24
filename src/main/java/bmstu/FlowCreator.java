a2e70c9ada18929473a64894dfc5c4071bff2707
furstepnik <furstepnik@yandex.ru>
2021-12-24 17:43:00 +0300

    Scripted auto-commit on change (2021-12-24 17:43:00) by gitwatch.sh

--- src/main/java/bmstu/FlowCreator.java
+++ src/main/java/bmstu/FlowCreator.java
@@ -1,4 +1,9 @@
 package bmstu;
 
 public class FlowCreator {
+    private static final String URL = "testUrl";
+    private static final String COUNT = "count";
+    private static final int NUMBER = 10;
+    private static final int DURATION = 5;
+    
 }
