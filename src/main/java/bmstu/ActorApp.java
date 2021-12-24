a96733e418b188b1e8b1ae92f004d70a918e61c7
furstepnik <furstepnik@yandex.ru>
2021-12-24 17:17:33 +0300

    Scripted auto-commit on change (2021-12-24 17:17:33) by gitwatch.sh

--- src/main/java/bmstu/ActorApp.java
+++ src/main/java/bmstu/ActorApp.java
@@ -1,5 +1,8 @@
 package bmstu;
 
+import com.sun.deploy.net.HttpRequest;
+import com.sun.deploy.net.HttpResponse;
+
 public class ActorApp {
     private static final String HOST = "localhost";
     private static final int PORT = 8080;
@@ -7,6 +10,11 @@ public class ActorApp {
     public static void main (String[] args) {
         ActorSystem system = ActorSystem.create("routes");
         ActorRef cache = system.actorOf(Props.create(ActorCache.class));
-        
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow;
+        try {
+            
+        }
     }
 }
