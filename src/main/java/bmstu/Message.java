b62fa38f6126ad42032499c4d7ef18859008be81
furstepnik <furstepnik@yandex.ru>
2021-12-24 18:07:06 +0300

    Scripted auto-commit on change (2021-12-24 18:07:06) by gitwatch.sh

--- src/main/java/bmstu/Message.java
+++ src/main/java/bmstu/Message.java
@@ -1,4 +1,11 @@
 package bmstu;
 
 public class Message {
+    private final String url;
+    public Message(String url) {
+        this.url = url;
+    }
+    public String getUrl() {
+        return this.url;
+    }
 }
