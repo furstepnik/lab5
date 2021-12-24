f441f77670085608dcf1f3b62243ebd646f6f407
furstepnik <furstepnik@yandex.ru>
2021-12-24 18:03:19 +0300

    Scripted auto-commit on change (2021-12-24 18:03:19) by gitwatch.sh

--- src/main/java/bmstu/FlowCreator.java
+++ src/main/java/bmstu/FlowCreator.java
@@ -73,8 +73,8 @@ public class FlowCreator {
                             }
                         }
                 )).map(resultPair -> {
-                    cache.tell(new MessageCache(resultPair.first(), resultPair.second()), ActorRef.noSender());
-                    return HttpResponse.create().withEntity(resultPair.first() + "\n avg time:  " + resultPair.second().toString() + "\n");
-                
+            cache.tell(new MessageCache(resultPair.first(), resultPair.second()), ActorRef.noSender());
+            return HttpResponse.create().withEntity(resultPair.first() + "\n avg time:  " + resultPair.second().toString() + "\n");
+        });
     }
 }
