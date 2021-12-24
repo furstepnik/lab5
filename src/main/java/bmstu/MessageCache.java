1e16e0eece2509db84a99edbd00aca988515d979
furstepnik <furstepnik@yandex.ru>
2021-12-24 18:09:46 +0300

    Scripted auto-commit on change (2021-12-24 18:09:46) by gitwatch.sh

--- src/main/java/bmstu/MessageCache.java
+++ src/main/java/bmstu/MessageCache.java
@@ -1,4 +1,16 @@
 package bmstu;
 
 public class MessageCache {
+    private final String url;
+    private final long time;
+    public MessageCache(String url, Long time) {
+        this.url = url;
+        this.time = time;
+    }
+    public String getUrl() {
+        return this.url;
+    }
+    public long getTime() {
+        return this.time;
+    }
 }
